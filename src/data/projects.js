240909c6e8c5a3c13b2998291ebda18c1d0dbc0a
SarahWanjiru <muhiawanjiru4@gmail.com>
2025-10-05 08:26:52 +0300

    refactor: removed old logos and config, added new assets and updated project details

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,14 +1,20 @@
 const projectsData = [
-  // Add your own projects here
-  // Example structure:
-  // {
-  //   id: 1,
-  //   image: projectImage,
-  //   title: "Your Project Name",
-  //   description: "Project description",
-  //   sourceCode: "https://github.com/yourusername/project",
-  //   liveProject: "https://yourproject.com",
-  // }
+  {
+    id: 1,
+    image: "/assets/elasticbeanstalk.jpeg",
+     title: "AWS Elastic Beanstalk Node.js Sample App",
+    description: "I created a Continuous Delivery Pipeline.",
+    sourceCode: "https://github.com/SarahWanjiru/aws-elastic-beanstalk-express-js-sample",
+    //liveProject: "https://yourproject.com",
+  },
+  {
+    id: 2,
+    image: "/assets/FundiFix.png",
+    title: "FundiFix - Revolutionizing how Kenya connects with skilled professionals",
+    description: "FundiFix connects Kenyans with skilled fundis (artisans) through WhatsApp.",
+    sourceCode: "https://github.com/SarahWanjiru/FundiFix",
+    liveProject: "https://drive.google.com/file/d/1gMN-GRmEu55uy6xf-4kr3ulvrl1nUJeD/view?usp=sharing",
+  },
 ];
 
 export default projectsData;
